1f02b528be795fec36ce318e7bdb958879649727
Valerio Sevilla <vsevilla@financialforce.com>
2017-04-08 22:36:32 +0200

    ADD First basic version of the show closet

---
+++ Assets/Drawer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : MonoBehaviour {
+
+	private static int OPEN_BOOL_HASH = Animator.StringToHash("Open");
+
+	private Animator anim;
+
+	public void open() {
+		anim.SetBool (OPEN_BOOL_HASH, true);
+	}
+
+	public void close() {
+		anim.SetBool (OPEN_BOOL_HASH, false);
+	}
+
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
