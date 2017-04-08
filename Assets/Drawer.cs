49a91084722b8f5a46039e86d7ef0ec6d51cbdcc
Valerio Sevilla <vsevilla@financialforce.com>
2017-04-08 23:14:58 +0200

    UPDATE Working Shoe Closet

--- Assets/Drawer.cs
+++ Assets/Drawer.cs
@@ -8,6 +8,13 @@ public class Drawer : MonoBehaviour {
 
 	private Animator anim;
 
+	public GameObject BlackShoePrefab;
+	public GameObject RedShoePrefab;
+	public GameObject BlueShoePrefab;
+	public GameObject WhiteShoePrefab;
+
+	private List<GameObject> Shoes;
+
 	public void open() {
 		anim.SetBool (OPEN_BOOL_HASH, true);
 	}
@@ -16,13 +23,53 @@ public class Drawer : MonoBehaviour {
 		anim.SetBool (OPEN_BOOL_HASH, false);
 	}
 
+	public void randomize() {
+		foreach (var shoe in Shoes) {
+			Destroy (shoe);
+		}
+
+		for (int i = 0; i < 3; ++i) {
+			int _random = Random.Range (1, 5);
+			GameObject shoe;
+			switch (_random) {
+			case 1:
+				shoe = Instantiate (BlackShoePrefab);
+				break;
+			case 2:
+				shoe = Instantiate (RedShoePrefab);
+				break;
+			case 3:
+				shoe = Instantiate (BlueShoePrefab);
+				break;
+			case 4:
+				shoe = Instantiate (WhiteShoePrefab);
+				break;
+			default:
+				shoe = Instantiate (BlackShoePrefab);
+				break;
+			}
+
+			shoe.transform.Rotate(new Vector3 (
+				-90.0f,
+				0.0f,
+				180.0f
+			));
+			shoe.transform.localPosition = new Vector3 (
+				-0.35f + (0.35f * i),
+				0.25f,
+				0.075f
+			);
+			shoe.transform.localScale = shoe.transform.localScale * 0.75f;
+
+			shoe.transform.SetParent (transform, false);
+		}
+			
+	}
+
 	// Use this for initialization
 	void Awake () {
 		anim = GetComponent<Animator> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		Shoes = new List<GameObject> ();
+		randomize ();
 	}
 }
---
+++ Assets/ShoeCloset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoeCloset : MonoBehaviour {
+
+	private List<Drawer> drawers;
+
+	// Use this for initialization
+	void Awake () {
+		drawers = new List<Drawer> ();
+
+		drawers.Add (transform.Find ("Drawer1").gameObject.GetComponent<Drawer> ());
+		drawers.Add (transform.Find ("Drawer2").gameObject.GetComponent<Drawer> ());
+		drawers.Add (transform.Find ("Drawer3").gameObject.GetComponent<Drawer> ());
+	}
+	
+	void Start () {
+//		foreach (var drawer in drawers) {
+//			drawer.open ();
+//		}
+	}
+}
