26bc4c2b5370e41703775583a5bcdf0ee6831813
Valerio Sevilla <vsevilla@financialforce.com>
2017-05-09 20:28:35 +0200

    ADD Game restart

---
+++ Assets/RestartScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			SceneManager.LoadScene ("MainScene");
+		}
+	}
+}
